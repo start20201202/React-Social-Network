00bf0c49a6c440eb9aa028b66df31f48cec36129
start20201202 <start20201202@gmail.com>
2021-09-20 00:32:10 +0300

    add Route path, BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
+import { Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
-/* import Profile from "./components/Profile/Profile";
- */
 const App = () => {
 	return (
-		<div className="app-wrapper">
-			<Header />
-			<Navbar />
-			{/* <Profile /> */}
-			<div className="app-wrapper-content">
-				<Dialogs />
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header />
+				<Navbar />
+				{/* <Profile /> */}
+				<div className="app-wrapper-content">
+					<Route path="/dialogs" component={Dialogs} />
+					<Route path="/profile" component={Profile} />
+				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 };
 
