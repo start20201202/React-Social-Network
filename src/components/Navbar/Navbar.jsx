4922720bbf87f26b499ed886b8e9b21a1c3dbce5
start20201202 <start20201202@gmail.com>
2021-09-20 00:43:29 +0300

    add color activeLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a href="/profile" >Profile</a>
+            <NavLink to="/profile" activeClassName={s.activeLink} >Profile</NavLink>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a href="/dialogs" >Messages</a>
+            <NavLink to="/dialogs" activeClassName={s.activeLink} >Messages</NavLink>
         </div>
         <div className={s.item}>
             <a>News</a>
