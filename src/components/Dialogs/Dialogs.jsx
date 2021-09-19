1e68e9d1dd89cf584094fe15e791eedcccc34597
start20201202 <start20201202@gmail.com>
2021-09-20 01:54:51 +0300

    create components DialogItem, Message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
-/* import {NavLink} from "react-router-dom";
- */
+
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return <div>
+
+    
+    <div className={s.dialog + ' '+ s.active}>
+    <NavLink to={path}>{props.name}</NavLink>
+</div>
+</div>
+}
+
+const Message =(props)=>{
+    return <div className={s.dialog}>{props.message}</div>
+
+}
+
 const Dialogs = (props) =>{
     return (
-        <div>
-Dialogs
+        <div className={s.dialogs} >
+            <div className={s.dialogsItems}>
+               <DialogItem name="Dimych" id="1"/>
+               <DialogItem name="Andrey" id="2"/>
+               <DialogItem name="Sveta" id="3"/>
+               <DialogItem name="Sasha" id="4"/>
+               <DialogItem name="Victor" id="5"/>
+               <DialogItem name="Valera" id="6"/>
+            </div>
+            <div className={s.messages}>
+<Message message="hi"/>
+<Message message="hello"/>
+<Message message="by"/>
+            </div>
         </div>
     )
 }
