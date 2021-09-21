ce974cadd0024cfd70f9cd0934994405966c6a8a
start20201202 <start20201202@gmail.com>
2021-09-21 16:02:34 +0300

    add ref newMessage

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,12 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} />  );
     let messagesElements = props.state.messages.map( m => <Message message={m.message}/> );
 
+let newMessage = React.createRef();
+let addMessage = () => {
+    let text = newMessage.current.value;
+    alert(text);
+}
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -15,7 +21,10 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 { messagesElements }
-            </div>
+                <textarea ref={newMessage}></textarea>
+                <button onClick={addMessage}>add message</button>
+          </div>
+             
         </div>
     )
 }
