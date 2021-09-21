92f69b646e228bb1085204145810ecbb4f31f6eb
start20201202 <start20201202@gmail.com>
2021-09-21 10:58:36 +0300

    create state.js and transfer data across props

--- src/App.js
+++ src/App.js
@@ -17,13 +17,11 @@ const App = (props) => {
 				<div className="app-wrapper-content">
 					<Route
 						path="/dialogs"
-						render={() => (
-							<Dialogs dialogs={props.dialogs} messages={props.messages} />
-						)}
+						render={() => <Dialogs state={props.state.dialogsPage} />}
 					/>
 					<Route
 						path="/profile"
-						render={() => <Profile posts={props.posts} />}
+						render={() => <Profile state={props.state.profilePage} />}
 					/>
 				</div>
 			</div>
--- src/index.js
+++ src/index.js
@@ -3,33 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-let posts = [
-	{ id: 1, message: "Hi, how are you?", likesCount: 12 },
-	{ id: 2, message: "It's my first post", likesCount: 11 },
-	{ id: 3, message: "Blabla", likesCount: 11 },
-	{ id: 4, message: "Dada", likesCount: 11 },
-];
-let dialogs = [
-	{ id: 1, name: "Dimych" },
-	{ id: 2, name: "Andrew" },
-	{ id: 3, name: "Sveta" },
-	{ id: 4, name: "Sasha" },
-	{ id: 5, name: "Viktor" },
-	{ id: 6, name: "Valera" },
-];
-
-let messages = [
-	{ id: 1, message: "Hi" },
-	{ id: 2, message: "How is your it-kamasutra?" },
-	{ id: 3, message: "Yo" },
-	{ id: 4, message: "Yo" },
-	{ id: 5, message: "Yo" },
-];
+import state from "./redux/state";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App posts={posts} dialogs={dialogs} messages={messages} />
+		<App state={state} />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
