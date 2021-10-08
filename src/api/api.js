113e5f9f46e4d5e173c603b8b23b7071033c2ae7
start20201202 <start20201202@gmail.com>
2021-10-08 15:43:06 +0300

    create const usersAPI

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+	withCredentials: true,
+	baseURL: "https://social-network.samuraijs.com/api/1.0/",
+	headers: {
+		"API-KEY": "907f74c9-4eb4-4d95-9b39-18ef5567e920",
+	},
+});
+
+export const usersAPI = {
+	getUsers(currentPage = 1, pageSize = 10) {
+		return instance
+			.get(`users?page=${currentPage}&count=${pageSize}`)
+			.then((response) => {
+				return response.data;
+			});
+	},
+};
